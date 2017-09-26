134a04ebb05acebf3d110f59be8604b190bc5edd
ltosc <ltosc@DESKTOP-C0BI8Q4>
2017-09-25 21:34:30 -0300

    Melhorias no tratamento dos valores ausentes e identificação de outliers

--- GameDBMining/GameDBMining.py
+++ GameDBMining/GameDBMining.py
@@ -1,13 +1,62 @@
 import pandas as pd
 import math
 
-# Carregando os dados
-dados = pd.read_csv("dados-07.csv")
+# Configurando avisos do Pandas
+pd.options.mode.chained_assignment = None
 
-# Substituindo valores 'tbd' por 'nan' no atributo 'avaliacao-usuarios'
-dados['avaliacao-usuarios']= dados['avaliacao-usuarios'].replace({'tbd':'-999'})
-dados['avaliacao-usuarios'] = dados['avaliacao-usuarios'].astype(float)
-dados['avaliacao-usuarios'] = dados['avaliacao-usuarios'].replace({-999:math.nan})
+# Carregando os dados e ajustando os tipos
+dados = pd.read_csv("dados-07.csv",
+                    dtype={ 
+                            "nome":"category",
+                            "plataforma":"category",
+                            "plataforma":"category",
+                            "genero":"category",
+                            "editora":"category",
+                            "lancamento":"category",
+                            "fabricante":"category",
+                            "vendas":"float",
+                            "avaliacao-criticos":"float",
+                            "numero-criticos":"float",
+                            "numero-usuarios":"float"
+                           }
+                    )
+
+# Ajustando inconsistência e tipo no atributo 'avaliacao-usuarios':
+# Substituição dos valores valores 'tbd' por 'nan' no atributo e conversão para float
+dados["avaliacao-usuarios"]= dados["avaliacao-usuarios"].replace({"tbd":"-999"})
+dados["avaliacao-usuarios"] = dados["avaliacao-usuarios"].astype(float)
+dados["avaliacao-usuarios"] = dados["avaliacao-usuarios"].replace({-999:math.nan})
+
+print()
+print("============================== ANTES DOS TRATAMENTOS ==============================")
+print()
+
+# Imprime os tipos dos dados
+print("=> Tipos dos dados:")
+dados_types = dados.dtypes
+print(dados_types)
+print()
+
+# Imprime a descrição dos dados
+print("=> Descrição dos dados:")
+dados_descr = dados.describe()
+print(dados_descr)
+print()
+
+# Imprime a quantidade de valores ausentes em cada atributo
+print("=> Valores ausentes:")
+print("nome: ", dados["nome"].isnull().sum())
+print("plataforma: ", dados["plataforma"].isnull().sum())
+print("genero: ", dados["genero"].isnull().sum())
+print("editora: ", dados["editora"].isnull().sum())
+print("lancamento: ", dados["lancamento"].isnull().sum())
+print("fabricante: ", dados["fabricante"].isnull().sum())
+print("vendas: ", dados["vendas"].isnull().sum())
+print("avaliacao-criticos: ", dados["avaliacao-criticos"].isnull().sum())
+print("numero-criticos: ", dados["numero-criticos"].isnull().sum())
+print("avaliacao-usuarios: ", dados["avaliacao-usuarios"].isnull().sum())
+print("numero-usuarios: ", dados["numero-usuarios"].isnull().sum())
+print()
 
 # Filtrando os dados por editora, gênero, quantidade de avaliações e avaliações
 dados_filter1 = dados[["editora", "genero", "numero-criticos", "avaliacao-criticos", "numero-usuarios", "avaliacao-usuarios"]]
@@ -28,20 +77,28 @@ dados_filter3 = dados_filter1_clean[["editora", "genero", "avaliacao-usuarios"]]
 dados_filter3_mean = dados_filter3.groupby(["editora", "genero"])["avaliacao-usuarios"].mean()
 
 # Faz a substituição dos valores ausentes de avaliação dos críticos pela média de avaliação por editora e gênero
+# Ou caso não existam dados para isto, substitui o valor ausente por um valor constante
 for i in range(0, len(dados_filter1.values)):
     game = dados_filter1.values[i]
     
-    if (math.isnan(game[3]) and ((game[0], game[1]) in dados_filter2_mean)):
-        game[3] = dados_filter2_mean[{game[0], game[1]}].values[0]
-        dados_filter1.set_value(i, 'avaliacao-criticos', game[3])
+    if math.isnan(game[3]):
+        if ((game[0], game[1]) in dados_filter2_mean):
+            game[3] = dados_filter2_mean[{game[0], game[1]}].values[0]
+            dados_filter1.set_value(i, "avaliacao-criticos", game[3])
+        else:
+            dados_filter1.set_value(i, "avaliacao-criticos", 0)
 
 # Faz a substituição dos valores ausentes de avaliação dos usuários pela média de avaliação por editora e gênero
+# Ou caso não existam dados para isto, substitui o valor ausente por um valor constante
 for i in range(0, len(dados_filter1.values)):
     game = dados_filter1.values[i]
     
-    if (math.isnan(game[5]) and ((game[0], game[1]) in dados_filter3_mean)):
-        game[5] = dados_filter3_mean[{game[0], game[1]}].values[0]
-        dados_filter1.set_value(i, 'avaliacao-usuarios', game[5])
+    if math.isnan(game[5]):
+        if ((game[0], game[1]) in dados_filter3_mean):
+            game[5] = dados_filter3_mean[{game[0], game[1]}].values[0]
+            dados_filter1.set_value(i, "avaliacao-usuarios", game[5])
+        else:
+            dados_filter1.set_value(i, "avaliacao-usuarios", 0)
         
 # Filtrando os dados_filter_1 por editora, gênero e quantidade de críticos
 dados_filter2 = dados_filter1_clean[["editora", "genero", "numero-criticos"]]
@@ -56,20 +113,28 @@ dados_filter3 = dados_filter1_clean[["editora", "genero", "numero-usuarios"]]
 dados_filter3_mean = dados_filter3.groupby(["editora", "genero"])["numero-usuarios"].mean()
         
 # Faz a substituição dos valores ausentes de avaliação dos críticos pela média de avaliação por editora e gênero
+# Ou caso não existam dados para isto, substitui o valor ausente por um valor constante
 for i in range(0, len(dados_filter1.values)):
     game = dados_filter1.values[i]
     
-    if (math.isnan(game[2]) and ((game[0], game[1]) in dados_filter2_mean)):
-        game[2] = dados_filter2_mean[{game[0], game[1]}].values[0]
-        dados_filter1.set_value(i, "numero-criticos", game[2])
+    if math.isnan(game[2]):
+        if ((game[0], game[1]) in dados_filter2_mean):
+            game[2] = dados_filter2_mean[{game[0], game[1]}].values[0]
+            dados_filter1.set_value(i, "numero-criticos", game[2])
+        else:
+            dados_filter1.set_value(i, "numero-criticos", 0)
 
 # Faz a substituição dos valores ausentes de avaliação dos usuários pela média de avaliação por editora e gênero
+# Ou caso não existam dados para isto, substitui o valor ausente por um valor constante
 for i in range(0, len(dados_filter1.values)):
     game = dados_filter1.values[i]
     
-    if (math.isnan(game[4]) and ((game[0], game[1]) in dados_filter3_mean)):
-        game[4] = dados_filter3_mean[{game[0], game[1]}].values[0]
-        dados_filter1.set_value(i, "numero-usuarios", game[4])
+    if math.isnan(game[4]):
+        if ((game[0], game[1]) in dados_filter3_mean):
+            game[4] = dados_filter3_mean[{game[0], game[1]}].values[0]
+            dados_filter1.set_value(i, "numero-usuarios", game[4])
+        else:
+            dados_filter1.set_value(i, "numero-usuarios", 0)
 
 # Ajusta os dados originais
 dados["numero-criticos"] = dados_filter1["numero-criticos"]
@@ -77,6 +142,107 @@ dados["avaliacao-criticos"] = dados_filter1["avaliacao-criticos"]
 dados["numero-usuarios"] = dados_filter1["numero-usuarios"]
 dados["avaliacao-usuarios"] = dados_filter1["avaliacao-usuarios"]
 
-dados_clean = dados.dropna(subset=["editora", "genero", "numero-criticos", "avaliacao-criticos", "numero-usuarios", "avaliacao-usuarios"])
+# Removendo outliers:
 
+# vendas
+dados_outlier_vendas = dados[["nome", "vendas"]]
+dados_filter4 = dados_outlier_vendas["vendas"]
+
+q1, q3 = dados_outlier_vendas["vendas"].quantile([0.25, 0.75])
+iqr = (q3-q1)
+min = q1 - (iqr * 1.5)
+max = q3 + (iqr * 1.5)
+
+dados_outlier_vendas["vendas"] = dados_filter4[(dados_filter4 > min) & (dados_filter4  < max)]
+dados_outlier_vendas = dados_outlier_vendas.loc[dados_outlier_vendas["vendas"].isnull()]
+dados_outlier_vendas = dados_outlier_vendas["nome"]
+
+# avaliacao-criticos
+dados_outlier_avaliacaocriticos = dados[["nome", "avaliacao-criticos"]]
+dados_filter4 = dados_outlier_avaliacaocriticos["avaliacao-criticos"]
+
+q1, q3 = dados_outlier_avaliacaocriticos["avaliacao-criticos"].quantile([0.25, 0.75])
+iqr = (q3-q1)
+min = q1 - (iqr * 1.5)
+max = q3 + (iqr * 1.5)
+
+dados_outlier_avaliacaocriticos["avaliacao-criticos"] = dados_filter4[(dados_filter4 > min) & (dados_filter4  < max)]
+dados_outlier_avaliacaocriticos = dados_outlier_avaliacaocriticos.loc[dados_outlier_avaliacaocriticos["avaliacao-criticos"].isnull()]
+dados_outlier_avaliacaocriticos = dados_outlier_avaliacaocriticos["nome"]
+
+# numero-criticos
+dados_outlier_numerocriticos = dados[["nome", "numero-criticos"]]
+dados_filter4 = dados_outlier_numerocriticos["numero-criticos"]
+
+q1, q3 = dados_outlier_numerocriticos["numero-criticos"].quantile([0.25, 0.75])
+iqr = (q3-q1)
+min = q1 - (iqr * 1.5)
+max = q3 + (iqr * 1.5)
+
+dados_outlier_numerocriticos["numero-criticos"] = dados_filter4[(dados_filter4 > min) & (dados_filter4  < max)]
+dados_outlier_numerocriticos = dados_outlier_numerocriticos.loc[dados_outlier_numerocriticos["numero-criticos"].isnull()]
+dados_outlier_numerocriticos = dados_outlier_numerocriticos["nome"]
+
+# avaliacao-usuarios
+dados_outlier_avaliacaousuarios = dados[["nome", "avaliacao-usuarios"]]
+dados_filter4 = dados_outlier_avaliacaousuarios["avaliacao-usuarios"]
+
+q1, q3 = dados_outlier_avaliacaousuarios["avaliacao-usuarios"].quantile([0.25, 0.75])
+iqr = (q3-q1)
+min = q1 - (iqr * 1.5)
+max = q3 + (iqr * 1.5)
+
+dados_outlier_avaliacaousuarios["avaliacao-usuarios"] = dados_filter4[(dados_filter4 > min) & (dados_filter4  < max)]
+dados_outlier_avaliacaousuarios = dados_outlier_avaliacaousuarios.loc[dados_outlier_avaliacaousuarios["avaliacao-usuarios"].isnull()]
+dados_outlier_avaliacaousuarios = dados_outlier_avaliacaousuarios["nome"]
+
+# numero-usuarios
+dados_outlier_numerousuarios = dados[["nome", "numero-usuarios"]]
+dados_filter4 = dados_outlier_numerousuarios["numero-usuarios"]
+
+q1, q3 = dados_outlier_numerousuarios["numero-usuarios"].quantile([0.25, 0.75])
+iqr = (q3-q1)
+min = q1 - (iqr * 1.5)
+max = q3 + (iqr * 1.5)
+
+dados_outlier_numerousuarios["numero-usuarios"] = dados_filter4[(dados_filter4 > min) & (dados_filter4  < max)]
+dados_outlier_numerousuarios = dados_outlier_numerousuarios.loc[dados_outlier_numerousuarios["numero-usuarios"].isnull()]
+dados_outlier_numerousuarios = dados_outlier_numerousuarios["nome"]
+
+print()
+print("============================== APÓS OS TRATAMENTOS ==============================")
+print()
+
+# Imprime a descrição dos dados após o tratamento
+print("=> Descrição dos dados:")
+dados_descr = dados.describe()
+print(dados_descr)
+print()
+
+# Imprime a quantidade de valores ausentes em cada atributo depois do tratamento
+print("=> Valores ausentes:")
+print("nome: ", dados["nome"].isnull().sum())
+print("plataforma: ", dados["plataforma"].isnull().sum())
+print("genero: ", dados["genero"].isnull().sum())
+print("editora: ", dados["editora"].isnull().sum())
+print("lancamento: ", dados["lancamento"].isnull().sum())
+print("fabricante: ", dados["fabricante"].isnull().sum())
+print("vendas: ", dados["vendas"].isnull().sum())
+print("avaliacao-criticos: ", dados["avaliacao-criticos"].isnull().sum())
+print("numero-criticos: ", dados["numero-criticos"].isnull().sum())
+print("avaliacao-usuarios: ", dados["avaliacao-usuarios"].isnull().sum())
+print("numero-usuarios: ", dados["numero-usuarios"].isnull().sum())
+print()
+
+# Imprime a quantidade de outliers
+print("=> Quantidade de outliers:")
+print("vendas: ", len(dados_outlier_vendas))
+print("avaliacao-criticos: ", len(dados_outlier_avaliacaocriticos))
+print("numero-criticos: ", len(dados_outlier_numerocriticos))
+print("avaliacao-usuarios: ", len(dados_outlier_avaliacaousuarios))
+print("numero-usuarios: ", len(dados_outlier_numerousuarios))
+print()
+
+# Liberando variáveis temporárias
 del dados_filter1, dados_filter1_clean, dados_filter2, dados_filter2_mean, dados_filter3, dados_filter3_mean, game, i
+del dados_filter4, q1, q3, iqr, min, max
